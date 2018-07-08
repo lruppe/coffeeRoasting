954f35a05392086a7f4805eba6a4e45fec52ab2b
U-LAPTOP-R41OV33Q\ltherupp <leon_ruppen@hotmail.com>
2018-07-08 17:49:32 +0200

    A

--- TalkToArduino_filter_v3.py
+++ TalkToArduino_filter_v3.py
@@ -21,58 +21,32 @@ Coffee roaster
 
 
 import sys
-
 import matplotlib
-
 import serial
-
 import time
-
 import io
-
 import Tkinter
-
 from Tkinter import *
-
 import pylab
-
 from pylab import *
-
-# import numpy
-
+import numpy
 import scipy as sp
-
 import scipy.signal as signal
-
-
-
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
-
 from matplotlib.figure import Figure
-
 import matplotlib.animation as animation
-
 from matplotlib import style
-
-
-
+matplotlib.use("TkAgg")
 import platform
 
-
-
 # prevents os x going to sleep mode
+if platform.system() == 'Darwin':
+    import caffeine
 
-import caffeine
-
-
-
-matplotlib.use("TkAgg")
+if platform.system() == 'Windows':
+    import winsound
 
 style.use('ggplot')
-
-
-
-
 
 temp_list=[0]
 
@@ -117,24 +91,7 @@ reftemp=np.hstack([np.hstack([np.hstack([np.hstack([np.hstack([np.hstack([np.hst
 reftemp2=np.hstack([np.hstack([np.hstack([np.hstack([np.linspace(0,180,3*60),np.linspace(180,210,3*60)]),np.linspace(210,230,30)]),np.linspace(230,250,6*60)]),np.zeros(120)])
 
 
-
-if platform.system() == 'Windows':
-
-    import winsound
-
-
-
-
-
-
-
-
-
 def main():
-
-
-
-
 
     def send_power_to_arduino(event):
 
@@ -142,13 +99,9 @@ def main():
 
         power=str(slider.get())
 
-
-
         if len(power)==1:
 
             power="0"+str(power)
-
-
 
         print(power)
 
@@ -158,27 +111,17 @@ def main():
 
         print(ser.read(ser.inWaiting()))
 
-
-
     def turn_power_off():
 
         slider.set(0)
 
         send_power_to_arduino(1)
 
-
-
     def _quit():
 
         root.quit()     # stops mainloop
 
         root.destroy()
-
-
-
-
-
-
 
     def get_temp():
 
@@ -188,33 +131,9 @@ def main():
 
         return ser.read(ser.inWaiting())
 
-
-
     def update_temp():
 
-        global power_list
-
-        global temp_list
-
-        global temp_list_filterd
-
-        global time_list
-
-        global t0
-
-        global timer_zeit
-
-        global P
-
-        global I
-
-        global pgain
-
-        global igain
-
-        global dgain
-
-        global reftemp
+        global power_list, temp_list, temp_list_filterd, time_list, t0,  timer_zeit, P, I, pgain, igain, dgain, reftemp
 
         print("temp updated")
 
@@ -225,8 +144,6 @@ def main():
         roestzeit=int(time.time()-t0)
 
         if controlling.get()==1 and temp_record==1:
-
-
 
             print "controlling"
 
@@ -244,17 +161,11 @@ def main():
 
                 Tc=int(reftemp[roestzeit])
 
-                # print "Tc=  ",Tc
-
-                # print "temp=  ", temp
-
                 E=Tc-int(temp)
 
                 print "E=  ", E
 
                 I=I+E
-
-                # print "I=  ",I
 
                 steig=0
 
@@ -268,15 +179,9 @@ def main():
 
                     D=0
 
-                # print "D=   ",D
-
                 P=P+E*pgain+I*igain+D*dgain
 
                 print "P =  " , P, "; Pgain= ", E*pgain, "; Igain= ", I*igain, "; Dgain= ", D*dgain, " Steigung= ",steig," Ref Steugung = ",(reftemp[roestzeit]-reftemp[roestzeit-d_bin])/d_bin
-
-
-
-
 
                 if P<=0:
 
@@ -286,35 +191,13 @@ def main():
 
                     P=100
 
-            # print "P   ="  , P
-
             slider.set(P)
 
             send_power_to_arduino(1)
 
-
-
-
-
         if temp_record==1:
 
             temp_list.append(abs(int(temp)))           #hier abs eingefuegt
-
-    #         if len(temp_list)>d_bin:
-
-    #             steig,b=np.polyfit(time_list[-d_bin:-1],temp_list[-d_bin:-1],1)
-
-    #             #temp_list_filterd.append(np.round(np.mean(temp_list[-d_bin:-1]),2)+steig*d_bin/2)
-
-    #             temp_list_filterd.append(steig*100)
-
-				# #temp_list_filterd=savitzky_golay(np.array((temp_list)),d_bin,3)
-
-
-
-    #         else:
-
-    #             temp_list_filterd.append(abs(int(temp)))
 
             if len(temp_list)>10:
 
@@ -324,29 +207,17 @@ def main():
 
                 temp_list_filterd = temp_list
 
-
-
-            #temp_list_filterd = (movingaverage(temp_list_filterd, points_mean))
-
             time_list.append(roestzeit)
 
             power_list.append(slider.get())
 
             label5.config(text="Roestzeit:   "+ str(roestzeit/60) + " min  "+str(mod(roestzeit,60))+" sec")
 
-
-
             line1[0].set_data(pylab.array(time_list[1:]),pylab.array(temp_list[1:]))
 
             line1[1].set_data(reftime,reftemp)
 
-            #line1[0].set_data(pylab.array(time_list[1:]),pylab.array(temp_list[1:]))
-
-
-
             line2[0].set_data(pylab.array(time_list[1:]),pylab.array(power_list[1:]))
-
-			
 
             line3[0].set_data(pylab.array(time_list[1:]),pylab.array(temp_list_filterd[1:]))
 
@@ -355,8 +226,6 @@ def main():
             line3[0].set_alpha(0.6)
 
             line3[0].set_color('b')
-
-
 
             if (time.time()-t0)/60.0>timer_zeit:
 
@@ -368,28 +237,13 @@ def main():
 
         rightFrame.after(900,update_temp)
 
-
-
     def start_temp_record():
 
-        global temp_record
-
-        global t0
+        global temp_record, t0
 
         t0=time.time()
 
         temp_record=1
-
-        temp_list=[0]
-
-        temp_list_filterd=[0]
-
-        time_list=[0]
-
-        power_list=[0]
-
-
-
 
 
     def stop_temp_record():
@@ -416,13 +270,11 @@ def main():
 
         thefile.write("temp_avg \n")
 
-        for item in temp_list_filterd:
+        # VAR temp_list_filtered not defined
 
-            thefile.write("%s\n" % item)
-
-
-
-
+        # for item in temp_list_filterd:
+        #
+        #     thefile.write("%s\n" % item)
 
         temp_record=0
 
@@ -430,21 +282,9 @@ def main():
 
         time_list=[0]
 
-        temp_list_filterd=[0]
-
-
-
     def timer_eingabe():
 
-        global timer_zeit
-
-        global dgain
-
-        global pgain
-
-        global igain
-
-        global d_bin
+        global timer_zeit, dgain, pgain, igain, d_bin
 
         timer_zeit = float(eingabe_timer.get())
 
@@ -458,28 +298,18 @@ def main():
 
         print("Timer auf "+str(timer_zeit)+" min gestellt und gains upgedated.")
 
-		
-
-
 
     def movingaverage(temp_data, points_mean):
 
         temp_data = np.asarray(temp_data, dtype=float)
 
-
-
         if len(temp_data)>points_mean:
-
-
 
             temp_data[-1] = numpy.mean(temp_data[-points_mean:len(temp_data)])
 
             temp_data = np.round(temp_data,2)
 
         return temp_data.tolist()
-
-
-
 
 
     def butterWorthFilter(temp_data):
@@ -492,17 +322,16 @@ def main():
 
         B, A = signal.butter(N, Wn, output='ba')
 
-         
-
         # Second, apply the filter
 
         temp_data = signal.filtfilt(B,A, temp_data)
 
-
-
         return temp_data
 
 
+    ###################################################################################################################
+    ##################################### START #######################################################################
+    ###################################################################################################################
 
     if platform.system() == 'Darwin':
 
@@ -517,21 +346,14 @@ def main():
                 pass
 
 
-
     if platform.system() == 'Windows':
 
         ser=serial.Serial('COM3',9600)
 
 
-
-
-
-
-
     print('wait 2 sec')
 
     time.sleep(2)
-
 
 
     root= Tk()
@@ -541,11 +363,9 @@ def main():
     root.wm_title("Coffe roaster")
 
 
-
     xAchse=pylab.arange(0,1,1)
 
     yAchse=pylab.array([0]*1)
-
 
 
     fig = pylab.figure(1)
@@ -563,7 +383,6 @@ def main():
     ax.axis([0,(18*60),0,300])
 
 
-
     reftime=pylab.arange(len(reftemp))
 
     line1=ax.plot(xAchse,yAchse,reftime,reftemp)
@@ -571,9 +390,6 @@ def main():
     line2=ax.plot(xAchse,yAchse,reftime,reftemp)
 
     line3=ax.plot(xAchse,yAchse,reftime,reftemp)
-
-
-
 
 
     leftFrame=Frame(root)
@@ -584,16 +400,7 @@ def main():
 
     rightFrame.pack(side=RIGHT)
 
-
-
-
-
-
-
-
-
     ### Temperatur anzeigen
-
     label4=Label(rightFrame,text="Temperatur:   "+ str(get_temp()) + " C")
 
     label4.pack()
@@ -603,19 +410,11 @@ def main():
     label5.pack()
 
 
-
-
-
-
-
-
-
     canvas = FigureCanvasTkAgg(fig, rightFrame)
 
     canvas.show()
 
     canvas.get_tk_widget().pack(side=BOTTOM, fill=BOTH, expand=True)
-
 
 
     toolbar = NavigationToolbar2TkAgg(canvas, rightFrame)
@@ -625,23 +424,14 @@ def main():
     canvas._tkcanvas.pack(side=TOP, fill=BOTH, expand=True)
 
 
-
-
-
-
-
     c = Checkbutton(leftFrame, text="Temperature controlling", variable=controlling,onvalue=1, offvalue=0)
 
     c.pack()
 
 
-
-
-
     label1=Label(leftFrame,text="Power")
 
     label1.pack()
-
 
 
     slider = Scale(leftFrame, from_=0, to=99, orient=VERTICAL)
@@ -655,40 +445,23 @@ def main():
     send_power_to_arduino(1)
 
 
-
-
-
-
-
-
-
     button_off=Tkinter.Button(leftFrame,text="Off",command=turn_power_off)
 
     button_off.pack()
-
-
 
     button_start=Tkinter.Button(leftFrame,text="Start",command=start_temp_record)
 
     button_start.pack()
 
-
-
     button_stop=Tkinter.Button(leftFrame,text="Stop",command=stop_temp_record)
 
     button_stop.pack()
-
-
 
     label6=Tkinter.Label(leftFrame,text="Timer in min (integer)")
 
     label6.pack()
 
-
-
     entryText10 = Tkinter.StringVar()
-
-
 
     eingabe_timer=Tkinter.Entry(leftFrame,textvariable=entryText10)
 
@@ -696,17 +469,11 @@ def main():
 
     eingabe_timer.pack()
 
-
-
     label7=Tkinter.Label(leftFrame,text="P Gain")
 
     label7.pack()
 
-
-
     entryText1 = Tkinter.StringVar()
-
-
 
     eingabe_p_gain=Tkinter.Entry(leftFrame,textvariable=entryText1)
 
@@ -714,17 +481,11 @@ def main():
 
     eingabe_p_gain.pack()
 
-
-
     label8=Tkinter.Label(leftFrame,text="D Gain")
 
     label8.pack()
 
-
-
     entryText2 = Tkinter.StringVar()
-
-
 
     eingabe_d_gain=Tkinter.Entry(leftFrame,textvariable=entryText2)
 
@@ -732,17 +493,11 @@ def main():
 
     eingabe_d_gain.pack()
 
-
-
     label9=Tkinter.Label(leftFrame,text="I Gain")
 
     label9.pack()
 
-
-
     entryText3 = Tkinter.StringVar()
-
-
 
     eingabe_i_gain=Tkinter.Entry(leftFrame,textvariable=entryText3)
 
@@ -750,19 +505,11 @@ def main():
 
     eingabe_i_gain.pack()
 
-
-
     label10=Tkinter.Label(leftFrame,text="d_bin")
 
     label10.pack()
 
-
-
-
-
     entryText4 = Tkinter.StringVar()
-
-
 
     eingabe_d_bin=Tkinter.Entry(leftFrame,textvariable=entryText4)
 
@@ -770,14 +517,9 @@ def main():
 
     eingabe_d_bin.pack()
 
-
-
     button_timer_ok=Tkinter.Button(leftFrame,text="OK",command=timer_eingabe)
 
     button_timer_ok.pack()
-
-
-
 
 
     root.protocol("WM_DELETE_WINDOW", _quit)  #thanks aurelienvlg
@@ -786,29 +528,11 @@ def main():
 
     root.mainloop()
 
-
-
-    #root.mainloop()
-
-
-
-
-
-
-
     ser.close()
 
-
-
     # releases any power assertion (os x can go sleep again)
-
-    caffeine.off()
-
-
-
-
-
-
+    if platform.system() == 'Darwin':
+        caffeine.off()
 
 if __name__ == "__main__":
 
